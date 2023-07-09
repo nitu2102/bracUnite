2375431fa3debe464a1844572cdc52327d9b4dd0
Farjana Alam <farjanalam2102@gmail.com>
2023-07-10 04:33:18 +0600

    Added signup view

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='Dashboard'),
-    path('home/', views.home, name='HOME'),
-    path('about', views.about, name='ABOUT'),
+    path('', views.home, name='HOME'),
+    path('dashboard/', views.dashboard, name='Dashboard'),
+    path('about/', views.about, name='ABOUT'),
     path('portfolio/', views.portfolio, name='PORTFOLIO'),
     path('study/', views.study, name='STUDY'),
     path('contact/', views.contact, name='CONTACT ME'),
--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, redirect
 
-# Create your views here.
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+#Add another import here
+
+#Email import
+# from django.core.mail import send_mail
+# from django.conf import settings
+# from django.core import mail
+# from django.core.mail.message import EmailMessage
+
 def dashboard(request):
-    return render(request, 'home.html')
+    return render(request, 'dashboard.html')
 def home(request):
     return render(request, 'home.html')
 def about(request):
@@ -17,6 +27,33 @@ def contact(request):
 
 def login(request):
     return render(request, 'login.html')
+
 def signup(request):
+    if request.method=="POST":
+        email = request.POST['email']
+        password = request.POST['password1']
+        confirm_password = request.POST['password2']
+        if password!=confirm_password:
+            messages.warning(request, "Password is not matching!")
+            return render(request, 'signup.html')
+
+        try:
+            if User.objects.get(username=email):
+                messages.warning(request, "Email is taken!")
+                return render(request, 'signup.html')
+
+        except Exception as identifier:
+            pass
+
+        myuser = User.objects.create_user(email, email, password)
+        myuser.save()
+        messages.info(request, "SignUp Successful! Please Login")
+        return redirect('/login')
+
     return render(request, 'signup.html')
+
+
+
+    return render(request, 'signup.html')
+
 
